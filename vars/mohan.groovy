924d52a0f52feec47727ae6bd6a8571e73d72422
MohanMarnattu <100912825+MohanMarnattu@users.noreply.github.com>
2024-03-16 23:22:58 +0530

    Update mohan.groovy

--- vars/mohan.groovy
+++ vars/mohan.groovy
@@ -4,3 +4,6 @@ def gitDownload(repo){
 def mavenBuild(){
   sh "mvn package"
 }
+def deploy(jobname,ip,path){
+  sh "scp /var/lib/jenkins/workspace/123/webapp/${jobname}/webapp.war ubuntu@$(ip):/var/lib/tomcat9/webapps/$(path).war"
+}
